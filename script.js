9650ee98bd2ffca311867241adb8fceeb1403220
jtieu7268 <77133493+jtieu7268@users.noreply.github.com>
2025-08-07 15:42:37 -0400

    Add function to get game state, whether someone has won, there is a tie, or game continues

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+const playerOneSymbol = 'X';
+const playerTwoSymbol = 'O';
+
 function Player(playerNumber, playerName) {
     const number = playerNumber;
     const name = playerName;
-    const symbol = number === 1 ? 'X' : 'O';
+    const symbol = number === 1 ? playerOneSymbol : playerTwoSymbol;
 
     const getNumber = () => number;
     const getName = () => name;
@@ -24,7 +27,11 @@ const gameController = (function GameController(
                 board[i].push('-');
             }
         }
-        
+
+        const isCellEmpty = (row, col) => board[row][col] === '-';
+
+        const getCell = (row, col) => board[row][col];
+
         const getBoard = () => board;
     
         const placeMove = (row, col, player) => {
@@ -38,7 +45,7 @@ const gameController = (function GameController(
                     + row.reduce((acc, cur) => acc + cur + " ", "") 
                     + "\n", ""));
     
-        return { getBoard, placeMove, printBoard };
+        return { isCellEmpty, getCell, getBoard, placeMove, printBoard };
     })();
 
     const players = [Player(1, playerOneName), Player(2, playerTwoName)];
@@ -55,12 +62,57 @@ const gameController = (function GameController(
 
     const playRound = (row, col) => {
         board.placeMove(row, col, getCurPlayer());
-        switchPlayer();
+        const gameState = getGameState();
+        if (gameState.state === "CONTINUE" ) {
+            switchPlayer();
+        } else {
+            return;
+        }
     }
 
-    printNewRound();
-    playRound(1,1);
-    printNewRound();
+    const getGameState = () => {
+        if (board.getCell(0,0) === board.getCell(1,0) && 
+            board.getCell(1,0) === board.getCell(2,0) &&
+            !board.isCellEmpty(0,0)) 
+            return { state: "WIN", winner: board.getCell(0,0)};
+        if (board.getCell(0,1) === board.getCell(1,1) && 
+            board.getCell(1,1) === board.getCell(2,1) &&
+            !board.isCellEmpty(0,1)) 
+            return { state: "WIN", winner: board.getCell(0,1)};
+        if (board.getCell(0,2) === board.getCell(1,2) && 
+            board.getCell(1,2) === board.getCell(2,2) &&
+            !board.isCellEmpty(0,2)) 
+            return { state: "WIN", winner: board.getCell(0,2)};
+
+        if (board.getCell(0,0) === board.getCell(1,0) && 
+            board.getCell(1,0) === board.getCell(2,0) &&
+            !board.isCellEmpty(0,0)) 
+            return { state: "WIN", winner: board.getCell(0,0)};
+        if (board.getCell(0,1) === board.getCell(1,1) && 
+            board.getCell(1,1) === board.getCell(2,1) &&
+            !board.isCellEmpty(0,1)) 
+            return { state: "WIN", winner: board.getCell(0,1)};
+        if (board.getCell(0,2) === board.getCell(1,2) && 
+            board.getCell(1,2) === board.getCell(2,2) &&
+            !board.isCellEmpty(0,2)) 
+            return { state: "WIN", winner: board.getCell(0,2)};
+
+        if (board.getCell(0,0) === board.getCell(1,1) && 
+            board.getCell(1,1) === board.getCell(2,2) &&
+            !board.isCellEmpty(0,0)) 
+            return { state: "WIN", winner: board.getCell(0,0) };
+        if (board.getCell(2,0) === board.getCell(1,1) && 
+            board.getCell(1,1) === board.getCell(0,2) &&
+            !board.isCellEmpty(2,0)) 
+            return { state: "WIN", winner: board.getCell(2,0) };
+
+        if (!board.getBoard().reduce((acc, row) => acc || row.reduce((acc, cell) => acc || cell === '-', false), false))
+            return { state: "TIE" };
+
+        return { state: "CONTINUE"};
+    }
+
+    console.log(getGameState(board.getBoard()));
 
     return { getCurPlayer }
 })();
